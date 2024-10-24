ec0ae3ddf3393bee7d9d258597d7a7cffdae52a7
Milos Rancic <milos.rancic@madetight.com>
2024-10-24 13:05:45 +0200

    form submit edit

--- src/js/kontaktaOss.js
+++ src/js/kontaktaOss.js
@@ -25,17 +25,17 @@ accordions.forEach(accordion => {
 
 // we observe the changes on the text in the submit button that is hidden
 // and apply that text to the button text that is visible
-const submitButtonText = document.querySelector("#offer-submit .button-text");
-const submitButton = document.querySelector("#offer-submit-button");
+const submitButtonText = document.querySelector("#contact-submit .button-text");
+const submitButtonWrapper = document.querySelector("#contact-submit-button");
 
 function updateDivContent() {
-  submitButtonText.textContent = submitButton.value;
+  submitButtonText.textContent = submitButtonWrapper.value;
 }
 
 
 // Observe for changes to the button text
 const observer = new MutationObserver(updateDivContent);
-observer.observe(submitButton, { attributes: true, attributeFilter: ['value'] });
+observer.observe(submitButtonWrapper, { attributes: true, attributeFilter: ['value'] });
 
 updateDivContent();
 
@@ -43,24 +43,25 @@ updateDivContent();
 // email confirm validation
 //////////
 function validateEmailConfirm() {
+  
   const email = document.getElementById('e-post-field').value;
   const emailConfirm = document.getElementById('e-post-confirm').value;
-  const submitButton = document.getElementById('offer-submit');
+  const submitButton = document.getElementById('contact-submit');
 
   // Disable validation if e-post-confirm is empty
   if (emailConfirm === '') {
       document.getElementById('e-post-confirm').classList.remove('invalid-mail');
-      submitButton.classList.remove('invalid-form');
+      submitButton.classList.remove('invalid-input');
       return; // Exit the function
   }
 
   // Perform validation if e-post-confirm is not empty
   if (email !== emailConfirm) {
       document.getElementById('e-post-confirm').classList.add('invalid-mail');
-      submitButton.classList.add('invalid-form');
+      submitButton.classList.add('invalid-input');
   } else {
       document.getElementById('e-post-confirm').classList.remove('invalid-mail');
-      submitButton.classList.remove('invalid-form');
+      submitButton.classList.remove('invalid-input');
   }
 }
 
--- src/js/offertformular.js
+++ src/js/offertformular.js
@@ -3,16 +3,16 @@ import '../css/offertformular.scss'
 // we observe the changes on the text in the submit button that is hidden
 // and apply that text to the button text that is visible
 const submitButtonText = document.querySelector("#offer-submit .button-text");
-const submitButton = document.querySelector("#offer-submit-button");
+const submitButtonWrapper = document.querySelector("#offer-submit-button");
 
 function updateDivContent() {
-  submitButtonText.textContent = submitButton.value;
+  submitButtonText.textContent = submitButtonWrapper.value;
 }
 
 
 // Observe for changes to the button text
 const observer = new MutationObserver(updateDivContent);
-observer.observe(submitButton, { attributes: true, attributeFilter: ['value'] });
+observer.observe(submitButtonWrapper, { attributes: true, attributeFilter: ['value'] });
 
 updateDivContent();
 
@@ -27,17 +27,17 @@ function validateEmailConfirm() {
   // Disable validation if e-post-confirm is empty
   if (emailConfirm === '') {
       document.getElementById('e-post-confirm').classList.remove('invalid-mail');
-      submitButton.classList.remove('invalid-form');
+      submitButton.classList.remove('invalid-input');
       return; // Exit the function
   }
 
   // Perform validation if e-post-confirm is not empty
   if (email !== emailConfirm) {
       document.getElementById('e-post-confirm').classList.add('invalid-mail');
-      submitButton.classList.add('invalid-form');
+      submitButton.classList.add('invalid-input');
   } else {
       document.getElementById('e-post-confirm').classList.remove('invalid-mail');
-      submitButton.classList.remove('invalid-form');
+      submitButton.classList.remove('invalid-input');
   }
 }
 
